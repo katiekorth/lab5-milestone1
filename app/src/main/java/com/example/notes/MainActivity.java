219d005f23fb885aa230a4f7ff76abfb0c7d5560
Katie Korth <kmkorth2@wisc.edu>
2021-10-24 20:51:21 -0500

    Added menu

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -2,9 +2,24 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickLogin(View view) {
+        EditText editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        String username = editTextUsername.getText().toString();
+        goToWelcome(username);
+    }
+
+    public void goToWelcome(String s) {
+        Intent intent = new Intent(this, Welcome.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/notes/Welcome.java
+++ app/src/main/java/com/example/notes/Welcome.java
@@ -2,13 +2,52 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.internal.NavigationMenuView;
 
 public class Welcome extends AppCompatActivity {
+
+    TextView textView2;
+    private NavigationMenuView welcomeMenuView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        textView2 = (TextView) findViewById(R.id.textViewWelcome);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("message");
+        textView2.setText("Welcome " + str + "!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void logoutFunction() {
+        Intent logoutIntent = new Intent(this, Welcome.class);
+        startActivity(logoutIntent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                logoutFunction();
+                return true;
+            case R.id.addNote:
+                return true;
+        }
+        return true;
     }
 }
